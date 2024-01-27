02b562868b4ab1cc16e34d80c0fc5c70793b9333
udodovrs <udodovrs@gmail.com>
2024-01-27 17:28:48 +0300

    task cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,20 @@
+import { spawn } from "node:child_process";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+console.log(__dirname);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  spawn("node", ["script.js", ...args], {
+    cwd: `${__dirname}/files`,
+    stdio: "inherit",
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess([
+  "Hi from child process",
+  "I am child process from script.js",
+  "Type CLOSE for exit",
+]);
